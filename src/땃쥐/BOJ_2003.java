106f428809fc2db720564a0515d473ca0f6eabc8
ttasjwi <ttasjwi920@gmail.com>
2022-03-11 21:07:41 +0900

    BOJ_2003 : 풀이1 (포인터 두개를 도입해서 이중 for문으로 처리)

---
+++ src/땃쥐/BOJ_2003.java
@@ -0,0 +1,40 @@
+package 땃쥐;
+
+import java.io.IOException;
+
+public class BOJ_2003 {
+
+    public static void main(String[] args) throws IOException {
+        int N = readInt();
+        int M = readInt();
+
+        int[] sums = new int[N+1];
+
+        for (int i=1; i<=N; i++) {
+            sums[i] = sums[i-1] + readInt();
+        }
+
+        int count = 0;
+        for (int point1=1; point1<=N; point1++) {
+            for (int point2=1; point2<=N; point2++) {
+                if (sums[point2] - sums[point1 - 1] == M) {
+                    count ++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+
+    private static int readInt() throws IOException {
+        int value = 0;
+        while (true) {
+            int input = System.in.read();
+            if (input == ' '||input == '\n') {
+                return value;
+            } else {
+                value = value * 10 + (input-48);
+            }
+        }
+
+    }
+}
