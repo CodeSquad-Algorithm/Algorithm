60e9fd1766f1a3c8aaf5805afdbc6335b214a310
ttasjwi <ttasjwi920@gmail.com>
2022-03-11 21:16:01 +0900

    BOJ_2003 - 불필요한 반복 제거
    - 2중 for문을 1중 for문으로 포인터 두개가 조건에 따라 이동하도록 함
    - 조건문을 배치하여 사용하여 불필요한 반복을 제거함

--- src/땃쥐/BOJ_2003.java
+++ src/땃쥐/BOJ_2003.java
@@ -8,18 +8,23 @@ public class BOJ_2003 {
         int N = readInt();
         int M = readInt();
 
-        int[] sums = new int[N+1];
+        int[] sums = new int[N + 1];
 
-        for (int i=1; i<=N; i++) {
-            sums[i] = sums[i-1] + readInt();
+        for (int i = 1; i <= N; i++) {
+            sums[i] = sums[i - 1] + readInt();
         }
 
         int count = 0;
-        for (int point1=1; point1<=N; point1++) {
-            for (int point2=1; point2<=N; point2++) {
-                if (sums[point2] - sums[point1 - 1] == M) {
-                    count ++;
+        for (int point1 = 1, point2 = 1; point1 <= N && point2 <= N; ) {
+            int sum = sums[point2] - sums[point1 - 1];
+            if (sum >= M) {
+                point1++;
+                point2 = point1;
+                if (sum == M) {
+                    count++;
                 }
+            } else {
+                point2++;
             }
         }
         System.out.print(count);
@@ -29,10 +34,10 @@ public class BOJ_2003 {
         int value = 0;
         while (true) {
             int input = System.in.read();
-            if (input == ' '||input == '\n') {
+            if (input == ' ' || input == '\n') {
                 return value;
             } else {
-                value = value * 10 + (input-48);
+                value = value * 10 + (input - 48);
             }
         }
 
